8a45dea7f7efa8598ba4d4d32873a3aaa8547a8e
Mech-Kal <kalpakkamble02@gmail.com>
2025-11-04 19:31:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function changeText() {
+      const para = document.getElementById("text");
+      // Replace <p> with <h1>
+      const heading = document.createElement("h1");
+      heading.textContent = "Entered Metaverse";
+      para.replaceWith(heading);
+    }
