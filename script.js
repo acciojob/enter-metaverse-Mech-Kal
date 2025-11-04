4985c9303794883ef6fdf01eeb64a78ade1ceac2
Mech-Kal <kalpakkamble02@gmail.com>
2025-11-04 20:11:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 const para = document.getElementById("text");
-const button = document.getElementById("btn");
-
-        // Attach the event listener to the button
+const button = document.getElementById("#enterBtn");
 button.addEventListener("click", function() {
-            
-            // 1. Create the new H1 element
-const heading = document.createElement("h1");
-            
-            // 2. Set the content
-heading.textContent = "Entered Metaverse";
-            
-            // 3. Replace the original paragraph with the new heading
-para.replaceWith(heading);
+	const heading = document.createElement("h1");
+	heading.textContent = "Entered Metaverse";
+    para.replaceWith(heading);
 });
